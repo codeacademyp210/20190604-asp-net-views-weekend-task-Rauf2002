714669ecad477daa575ffd472514b708a32e87c8
RaufSa@code.edu.az <RaufSa@code.edu.az>
2019-06-09 14:08:00 +0400

    Initialize

---
+++ Demos/Demos/Demo1/Controllers/WorksController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo1.Controllers
+{
+    public class WorksController : Controller
+    {
+        // GET: Works
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult P2()
+        {
+            return View();
+        }
+    }
+}
---
+++ Demos/Demos/Demo14/Controllers/CaseController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo14.Controllers
+{
+    public class CaseController : Controller
+    {
+        // GET: Case
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult P2()
+        {
+            return View();
+        }
+        public ActionResult Por1()
+        {
+            return View();
+        }
+        public ActionResult Por2()
+        {
+            return View();
+        }
+        public ActionResult Por3()
+        {
+            return View();
+        }
+        public ActionResult Por4()
+        {
+            return View();
+        }
+        public ActionResult Por5()
+        {
+            return View();
+        }
+        public ActionResult Por6()
+        {
+            return View();
+        }
+        public ActionResult Por21()
+        {
+            return View();
+        }
+        public ActionResult Por22()
+        {
+            return View();
+        }
+        public ActionResult Por23()
+        {
+            return View();
+        }
+        public ActionResult Por24()
+        {
+            return View();
+        }
+    }
+}
